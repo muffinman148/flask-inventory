cc25d4b548bf428cf0d3048608df27b9b8dfb21d
muffinman148 <pandamcg@gmail.com>
2018-06-07 12:09:08 -0700

    Logging, Email, Styles - Adds logging features for admin user. Adds colorized logging for simple assessment of log. Fixes email issues and logging issues.

--- inventory.py
+++ inventory.py
@@ -1,7 +1,7 @@
-# from app import app
 from app import app, db
 from app.models import User, Measurements
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Measurements': Measurements}
+    return {'db': db, 'User': User, 'Measurements': Measurements, 
+            'Items' : Items}
