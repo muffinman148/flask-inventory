604ab643eeba73c3aeb82b166fde264863050a1a
muffinman148 <pandamcg@gmail.com>
2019-07-10 08:05:20 -0700

    Footer, Model Addition - Adds a context processor for supplying year to footer. Adds model for Items.

--- inventory.py
+++ inventory.py
@@ -1,9 +1,18 @@
 from app import create_app, db
-from app.models import User, Measurements
+from app.models import User, Measurements, Items
+from datetime import datetime
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
+    """Registers the following items when flask shell is run."""
+
     return {'db': db, 'User': User, 'Measurements': Measurements, 
             'Items' : Items}
+
+@app.context_processor
+def inject_current_year():
+    """Returns current year for all pages."""
+
+    return {'now': datetime.utcnow()}
