f2a8c3c68872f1e661b666792bd4495677835146
muffinman148 <pandamcg@gmail.com>
2019-07-10 08:01:05 -0700

    SQLAlchemy Config - Removes SQLALCHEMY_BINDS due to a potential issue with the SQLAlchemy library.

--- config.py
+++ config.py
@@ -8,6 +8,16 @@ class Config(object):
     SECRET_KEY = os.getenv("SECRET_KEY")
     SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # TODO Fix SQLALCHEMY_BINDS Usage
+    #
+    # The following line will create a "TypeError: string indices must be
+    # integers" Error that will break the site. Binds are used for multiple
+    # databases to be referenced using SQLALCHEMY. This may be fixed in later
+    # builds.
+    #
+    #  SQLALCHEMY_BINDS = os.getenv("SQLALCHEMY_BINDS")
+
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
